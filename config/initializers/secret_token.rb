aad96ca4976ba63c38d0f8e1c9160eff70571343
PWiseman <passbyreference0@gmail.com>
2011-11-13 20:53:31 -0500

    Creating my first app checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2c9745cf5cf7f6f2b4f2e42bbb019d0a103ee6e072d9bb5512edfac9e64a63289c59f8ed6caa9525dd391991d5ab841ad008b69c32c226b4dda71fbcbb35c131'
